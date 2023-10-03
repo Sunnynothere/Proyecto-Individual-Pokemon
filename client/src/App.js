54d2183e1546cfb29f89f2f216420169203ed60f
Sol Lizarraga Grigoni <grigonisol@hotmail.com>
2023-10-03 19:19:03 -0300

    deployed

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Form from './components/Form';
 import Home from './components/Home';
 import Details from './components/Details';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:3005';
+axios.defaults.baseURL = 'https://pi-pokemon-henry.netlify.app/';
 
 function App() {
   return (
